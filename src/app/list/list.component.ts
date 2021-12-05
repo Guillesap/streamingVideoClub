18e6f05881ce7e5ebee29c8179c64d5fa3be704d
WillyS <hsapag@hotmail.com>
2021-12-05 13:53:21 -0300

    [agregando lista]

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -10,16 +10,16 @@ import { Component, OnInit } from '@angular/core';
 export class ListComponent  {
 
     estrenos = [
-      {numero:'https://miradetodo.co/wp-content/uploads/2021/11/tt13432484.jpg',name:'Zeros and Ones',id:'2021', price:'$400'},
-      {numero:'https://miradetodo.co/wp-content/uploads/2021/11/tt13432484.jpg',name:'Mic',id:'2021', price:'$500'},
-      {numero:'../assets/images/monitor.jpg', name:'Monitor',id:'2021', price:'$450'},
-      {numero:'../assets/images/mouse.jpg', name:'Mouse',id:'2021', price:'$500'},
-      {numero:'../assets/images/mouse.jpg', name:'Mouse',id:'2021', price:'$450'},
-      {numero:'../assets/images/mouse.jpg', name:'Mouse',id:'2021', price:'$500'},
-      {numero:'../assets/images/mouse.jpg', name:'Mouse',id:'2021', price:'$450'},
-      {numero:'../assets/images/mouse.jpg', name:'Mouse',id:'2021', price:'$500'},
-      {numero:'../assets/images/mouse.jpg', name:'Mouse',id:'2021', price:'$600'},
-      {numero:'../assets/images/mouse.jpg', name:'Mouse',id:'2021', price:'$500'},
+      {portada:'https://miradetodo.co/wp-content/uploads/2021/11/tt3420504.jpg',name:'Finch',id:'2021', price:'$600'},
+      {portada:'https://miradetodo.co/wp-content/uploads/2021/12/tt11628854.jpg',name:'Selent Night',id:'2021', price:'$500'},
+      {portada:'https://miradetodo.co/wp-content/uploads/2021/11/tt11448490.jpg', name:'Masquerade',id:'2021', price:'$450'},
+      {portada:'https://miradetodo.co/wp-content/uploads/2021/11/tt4244994.jpg', name:'The Last Duel',id:'2021', price:'$500'},
+      {portada:'https://miradetodo.co/wp-content/uploads/2021/11/tt11649338.jpg', name:'Black Friday',id:'2021', price:'$450'},
+      {portada:'https://miradetodo.co/wp-content/uploads/2021/10/tt12383460.jpg', name:'Hypnotic',id:'2021', price:'$500'},
+      {portada:'https://miradetodo.co/wp-content/uploads/2021/11/tt7097896.jpg', name:'Venom',id:'2021', price:'$450'},
+      {portada:'https://miradetodo.co/wp-content/uploads/2021/10/tt11686490.jpg', name:'The Deep House',id:'2021', price:'$500'},
+      {portada:'https://miradetodo.co/wp-content/uploads/2021/10/tt14867006.jpg', name:'VHS 94',id:'2021', price:'$600'},
+      {portada:'https://miradetodo.co/wp-content/uploads/2021/10/tt12060172.jpg', name:'The mutation',id:'2021', price:'$500'},
     ];
 
      }
