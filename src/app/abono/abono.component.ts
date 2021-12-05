8903248ae4620d01f0bda4ce45ce905e85480cb3
WillyS <hsapag@hotmail.com>
2021-12-05 01:35:26 -0300

    [agregando seleccion de abonos]

--- src/app/abono/abono.component.ts
+++ src/app/abono/abono.component.ts
@@ -10,6 +10,17 @@ export class AbonoComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    $('#sTandar').on('click', () => {
+      alert("Estas por comprar el  Abono Standard, por $ 2.000,00.- mensuales. Finalizá tu compra en el botón verde ingresando tu tarjeta de crédito")
+      });
+
+     $('#pRemium').on('click', () => {
+      alert("Estas por comprar el  Abono Premium, por $ 3.000,00.- mensuales. Finalizá tu compra en el botón verde ingresando tu tarjeta de crédito")
+      });
+    $('#gOld').on('click', () => {
+      alert("Estas por comprar el  Abono Gold, por $ 4.000,00.- mensuales. Finalizá tu compra en el botón verde ingresando tu tarjeta de crédito")
+      });
+    }
   }
 
-}
+
