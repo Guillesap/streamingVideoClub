21debc57a17309c900d27cc10fad8eff5c7f21f4
WillyS <hsapag@hotmail.com>
2021-12-05 00:29:57 -0300

    [modificando login]

--- src/app/carrito/carrito.component.ts
+++ src/app/carrito/carrito.component.ts
@@ -8,37 +8,13 @@ import * as $ from "jquery";
 })
 export class CarritoComponent implements OnInit {
 
-  constructor() { }
 
   ngOnInit(): void {
-    window.onload = function () {
-
-      const Cuadros = [
-          {
-              id: 1,
-              nombre: 'Abono Standard',
-              precio: 2000,
-          },
-          {
-              id: 2,
-              nombre: 'Abono Premium',
-              precio: 3000,
-          },
-          {
-              id: 3,
-              nombre: 'Abono Gold',
-              precio: 4000,
-          },
-      ];
-    }
-
-  //boton finalizar compra
-  $('#finalizarComprar').on('click', () => {
+    $('#finalizarComprar').on('click', () => {
       alert("Gracias por tu compra. A disfrutar tu Abono!");
       });
-
+    }
   }
 
-}
 
 
